1b010c17ae79d8dddc004c3a310182cc4568bf99
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-07-23 20:34:44 -0300

    Implementing a gettime()

--- gettime.c
+++ gettime.c
@@ -1,11 +1,16 @@
 #include <gettime.h>
 
+UINT64 _usec;
+
 void gettime(int *sec, int *usec)
 {
   EFI_TIME time;
   EFI_TIME_CAPABILITIES time_cap;
   RS->GetTime(&time, &time_cap);
 
-  *sec = time.Second;
-  *usec += time.Second * 1000000;
+  *sec = time.Hour * 3600 + time.Minute * 60 + time.Second;
+  _usec += 5000;
+  if(_usec >= 1000000)
+    _usec = 0;
+  *usec = _usec;
 }
